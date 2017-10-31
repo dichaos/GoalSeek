df857af5599cdfb23e6b20b5b2c7d4d3d1ffad2e
Dikaios Papadogkonas <dikaios.papadogkonas@oakam.com>
2017-10-31 12:26:32 +0000

    Algorithms

---
+++ GoalSeek/BisectionMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoalSeek
+{
+    public class BisectionMethod : IGoalSeek
+    {
+        public delegate double Function(double x);
+
+        public double Seek(double TOL, int MaxIterations, double a, double b, double seekValue, Function f)
+        {
+            for(int i = 0; i < MaxIterations; i++)
+            {
+                double c = (a + b) / 2d;
+
+                double fc = f(c);
+                double fa = f(a);
+                
+                if (fc == seekValue || (b-a)/2d < TOL)
+                {
+                    return c;
+                }
+
+                if ((fc < seekValue && fa < seekValue) || (fc > seekValue && fa > seekValue))
+                    a = c; 
+                else
+                    b = c;
+            }
+
+            return double.NaN;
+        }
+    }
+}
---
+++ GoalSeek/NewtonMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoalSeek
+{
+    public class NewtonMethod : IGoalSeek
+    {
+        public delegate double Function(double x);
+
+        public double Seek(double seekValue, int maxIterations, double a, double cutOff, Function f, Function derF)
+        {
+            double result = double.MaxValue;
+            double x = a;
+            double newX = x;
+            int count = 0;
+
+            while (Math.Abs(seekValue - result) > cutOff && count < maxIterations)
+            {
+                newX = x - (f(x) / derF(x));
+                result = f(x);
+            }
+
+            return result;
+        }
+    }
+}
